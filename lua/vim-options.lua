09a0efdb2a141db9b050fd83b69a50a3a61d6e6c
Fernando Rosa <fernandosvrosa@gmail.com>
2025-04-30 00:32:45 -0300

    format

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,26 +8,25 @@ vim.g.background = "light"
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 
-if string.find(vim.fn.system('uname -r'), 'WSL') then
-  vim.opt.clipboard = "unnamedplus"
-  vim.g.clipboard = {
-    name = 'WslClipboard',
-    copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
-    },
-    paste = {
-      ['+'] = 'powershell.exe -c Get-Clipboard',
-      ['*'] = 'powershell.exe -c Get-Clipboard',
-    },
-  }
+if string.find(vim.fn.system("uname -r"), "WSL") then
+	vim.opt.clipboard = "unnamedplus"
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = "powershell.exe -c Get-Clipboard",
+			["*"] = "powershell.exe -c Get-Clipboard",
+		},
+	}
 end
-
