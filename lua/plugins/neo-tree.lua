e820a3ef0cfdebf7e9a27d7500256fe4c5b43297
Fernando Rosa <fernandosvrosa@gmail.com>
2025-04-30 00:31:40 -0300

    format

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,17 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  lazy = false,   
-  opts = {
-  },
-  config = function()
-    -- vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
-  end
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		lazy = false,
+		opts = {},
+		config = function()
+			-- vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+			vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+		end,
+	},
 }
