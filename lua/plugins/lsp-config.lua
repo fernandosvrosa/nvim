2bf8f3cda90402ee485978ed6d1ca6ede34c3018
Fernando s Rosa <fernandosvrosa@gmail.com>
2025-04-30 08:17:14 -0300

    formartter

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})                      --ver a documentação
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})                -- Ir para definição
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})               -- Ir para declaração
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- Ações de código
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) --ver a documentação
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- Ir para definição
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {}) -- Ir para declaração
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}) -- Ações de código
+		end,
+	},
 }
