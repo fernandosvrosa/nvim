57a05754e32fbf9813e53dabea9d74469b84af2d
Fernando Rosa <fernandosvrosa@gmail.com>
2025-05-05 23:38:01 -0300

    add plugin for add comment in codes

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,16 @@
+return {
+  {
+  "numToStr/Comment.nvim",
+  opts = {
+    -- configuração padrão funciona bem
+    -- | Modo   | Ação                                   | Atalho                           |
+    -- | ------ | -------------------------------------- | -------------------------------- |
+    -- | Normal | Comentar a linha atual                 | `gcc`                            |
+    -- | Normal | Comentar várias linhas (com movimento) | `gc3j` (comenta 3 linhas abaixo) |
+    -- | Visual | Comentar seleção                       | `gc`                             |
+    -- | Visual | Comentar em bloco (se suportado)       | `gb`                             |
+  },
+  lazy = false,
+}
+}
+
