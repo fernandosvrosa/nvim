c8652e963772a2f02b7881d8f1b819bbfad34fb4
Fernando Rosa <fernandosvrosa@gmail.com>
2026-03-16 19:08:37 -0300

    feat: add hidden folder in file explorerwq

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
       local oil = require("oil")
       oil.setup({
         default_file_explorer = false, -- para manter Neo-tree como default
+        view_options = {
+          show_hidden = true,
+        },
       })
       vim.keymap.set("n", "-", oil.toggle_float, {})
     end,
